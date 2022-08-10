840c874c4acf0babf3e0a1a91965319246864438
ggstud <110525696+ggstud@users.noreply.github.com>
2022-08-10 13:27:39 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void bubble_sort(int arr[], int sz) {
+	int i = 0;
+	for (i = 0; i < sz - 1; i++) {
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+
+}
+int main(void) {
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
+	int i = 0;
+	for (i = 0; i < sz; i++) {
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
